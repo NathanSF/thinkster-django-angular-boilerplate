dfac0acb14444c66599e63224148a6136a799917
Nathan Frankel <frankel.nathan@gmail.com>
2016-01-06 19:24:11 -0800

    Fix redirect on register bug

--- static/javascripts/authentication/services/authentication.service.js
+++ static/javascripts/authentication/services/authentication.service.js
@@ -35,17 +35,31 @@
         * @name register
         * @desc Try to register a new user
         * @param {string} username The username entered by the user
-        * @param {string} password The password entered by the user
-        * @param {string} email The email entered by the user
+        * @param {string} password The  password entered by the user
+        * @param {string} email The email  entered by the user
         * @returns {Promise}
-        * @memberOf thinkster.authentication.services.Authentication
-        */
+        * @memberOf    thinkster.authentication.services.Authentication
+        **/
         function register(email, password, username) {
-          return $http.post('/api/v1/accounts/', {
-            username: username,
-            password: password,
-            email: email
-          });
+            return $http.post('/api/v1/accounts/', {
+                username: username,
+                password: password,
+                email: email
+            }).then(registerSuccessFn, registerErrorFn);
+            /**
+            * @name registerSuccessFn
+            * @desc Log the new user in
+            */
+            function registerSuccessFn(data, status, headers, config) {
+                Authentication.login(email, password);
+            }
+            /**
+             * @name registerErrorFn
+             * @desc Log "Epic failure!" to the console
+             */
+            function registerErrorFn(data, status, headers, config) {
+                console.error('Epic Failure!');
+            }
         }
 
         /**
